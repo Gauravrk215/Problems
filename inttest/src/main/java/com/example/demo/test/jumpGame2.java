90079867257aae9a250cf9f833f4007e672075c6
gauravrk215 <gauravrk215@gmail.com>
2024-10-28 23:45:36 +0530

    inttoroman

--- inttest/src/main/java/com/example/demo/test/jumpGame2.java
+++ inttest/src/main/java/com/example/demo/test/jumpGame2.java
@@ -2,6 +2,11 @@ package com.example.demo.test;
 
 public class jumpGame2 {
 
+//	You are given an integer array nums. 
+//	You are initially positioned at the array's first index,
+//	and each element in the array represents your maximum jump length at that position.
+//  Return true if you can reach the last index, or false otherwise.
+
 	public boolean canJump(int[] nums) {
 
 		int maxReach = 0; // ab tak ka maximum reachable index
---
+++ inttest/src/main/java/com/example/demo/test2/IntegerToRoman.java
@@ -0,0 +1,53 @@
+package com.example.demo.test2;
+
+import java.util.HashMap;
+
+public class IntegerToRoman {
+
+	public int romanToInt(String s) {
+
+		int out = 0;
+
+		int length = s.length();
+
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		map.put('I', 1);
+		map.put('V', 5);
+		map.put('X', 10);
+		map.put('L', 50);
+		map.put('C', 100);
+		map.put('D', 500);
+		map.put('M', 1000);
+
+		for (int i = 0; i < length; i++) {
+
+			char chr = s.charAt(i);
+
+			Integer cvalue = map.get(chr);
+
+			if (i < length - 1 && cvalue < map.get(s.charAt(i + 1))) {
+				out -= cvalue;
+			} else {
+
+				out += cvalue;
+			}
+
+		}
+
+		return out;
+
+	}
+
+	public static void main(String[] args) {
+
+		IntegerToRoman data = new IntegerToRoman();
+		int test1 = data.romanToInt("DCXXI");
+		System.out.println(test1);
+
+		int test2 = data.romanToInt("IV");
+		System.out.println(test2);
+
+	}
+
+}
