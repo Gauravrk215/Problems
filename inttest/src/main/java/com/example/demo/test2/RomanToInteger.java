15e8e747de535555ec29ccda5f7d955053c03bbc
gauravrk215 <gauravrk215@gmail.com>
2024-10-28 23:46:18 +0530

    ittoroman

--- inttest/src/main/java/com/example/demo/test2/RomanToInteger.java
+++ inttest/src/main/java/com/example/demo/test2/RomanToInteger.java
@@ -4,47 +4,32 @@ import java.util.HashMap;
 
 public class RomanToInteger {
 
-	public int romanToInt(String s) {
+	public String intToRoman(int num) {
+		// Define values and corresponding Roman symbols
+		int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+		String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-		int out = 0;
+		StringBuilder roman = new StringBuilder();
 
-		int length = s.length();
-
-		HashMap<Character, Integer> map = new HashMap<>();
-
-		map.put('I', 1);
-		map.put('V', 5);
-		map.put('X', 10);
-		map.put('L', 50);
-		map.put('C', 100);
-		map.put('M', 1000);
-
-		for (int i = 0; i < length; i++) {
-
-			char chr = s.charAt(i);
-
-			Integer cvalue = map.get(chr);
-
-			if (i < length - 1 && cvalue < map.get(s.charAt(i + 1))) {
-				out -= cvalue;
-			} else {
-
-				out += cvalue;
+		// Iterate over each value-symbol pair
+		for (int i = 0; i < values.length; i++) {
+			// Determine how many times to append the current symbol
+			while (num >= values[i]) {
+				roman.append(symbols[i]); // Append symbol
+				num -= values[i]; // Subtract value
 			}
-
 		}
 
-		return out;
-
+		return roman.toString();
 	}
 
 	public static void main(String[] args) {
 
 		RomanToInteger data = new RomanToInteger();
-		int test1 = data.romanToInt("DCXXI");
+		String test1 = data.intToRoman(3749);
 		System.out.println(test1);
 
-		int test2 = data.romanToInt("IV");
+		String test2 = data.intToRoman(58);
 		System.out.println(test2);
 
 	}
