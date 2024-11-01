8c4dc388d659815a04a7f188e3e3f38fc6cf625e
gauravrk215 <gauravrk215@gmail.com>
2024-11-01 09:02:28 +0530

    new

---
+++ inttest/src/main/java/com/example/demo/test2/ZigzagConversion.java
@@ -0,0 +1,28 @@
+package com.example.demo.test2;
+
+public class ZigzagConversion {
+
+	public String convert(String s, int numRows) {
+
+		String output = "";
+		
+		
+		
+
+		return output;
+	}
+
+	public static void main(String[] args) {
+
+		ZigzagConversion data = new ZigzagConversion();
+
+		String reverseWords = data.convert("PAYPALISHIRING", 3);
+
+		System.out.println(reverseWords);
+
+		String reverseWords2 = data.convert("PAYPALISHIRING", 4);
+
+		System.out.println(reverseWords2);
+	}
+
+}
