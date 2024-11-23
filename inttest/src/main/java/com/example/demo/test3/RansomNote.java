1855d443ea8aaf945ae6c48b81d1c77a30010eb0
Gaurav Kumar <gauravrk215@gmail.com>
2024-11-23 23:59:36 +0530

    Update RansomNote.java

--- inttest/src/main/java/com/example/demo/test3/RansomNote.java
+++ inttest/src/main/java/com/example/demo/test3/RansomNote.java
@@ -1,22 +1,35 @@
 package com.example.demo.test3;
 
+import java.util.HashMap;
+
 public class RansomNote {
 
 	public static boolean canConstruct(String ransomNote, String magazine) {
 
 		int rlength = ransomNote.length();
-		if (rlength>magazine.length() ) {
+		int mlength = magazine.length();
+		if (magazine.length() == 0) {
 			return false;
 
+		}
+		HashMap<Character, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < mlength; i++) {
+
+			map.put(magazine.charAt(i), map.getOrDefault(magazine.charAt(i), 0) + 1);
 		}
 
 		for (int i = 0; i < rlength; i++) {
 
 			char charAt = ransomNote.charAt(i);
-			char charAt2 = magazine.charAt(i);
-			if (charAt != charAt2) {
-				return false;
 
+			Integer integer = map.get(charAt);
+
+			if (integer == null || integer == 0) {
+				return false;
+			} else if (integer >= 1) {
+
+				map.put(charAt, integer - 1);
 			}
 
 		}
@@ -26,10 +39,13 @@ public class RansomNote {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		boolean canConstruct1 = canConstruct("aa", "aab");
-		System.out.println(canConstruct1);
+//		boolean canConstruct2 = canConstruct("a", "b");
+//		System.out.println(canConstruct2);
+//		
+//		boolean canConstruct1 = canConstruct("aab", "baa");
+//		System.out.println(canConstruct1);
 
-		boolean canConstruct = canConstruct("a", "b");
+		boolean canConstruct = canConstruct("aa", "ab");
 		System.out.println(canConstruct);
 
 	}
