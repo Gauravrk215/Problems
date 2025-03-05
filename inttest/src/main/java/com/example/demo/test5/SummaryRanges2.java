e28c4f33ba433781eeb5f751b50169350f949587
Gaurav Kumar <GAURAVRK215@GMAIL.COM>
2025-03-05 23:46:38 +0530

    summary range

--- inttest/src/main/java/com/example/demo/test5/SummaryRanges2.java
+++ inttest/src/main/java/com/example/demo/test5/SummaryRanges2.java
@@ -3,52 +3,42 @@ package com.example.demo.test5;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SummaryRanges {
+public class SummaryRanges2 {
 	public List<String> summaryRanges(int[] nums) {
 		List<String> result = new ArrayList<>();
 
 		if (nums == null || nums.length == 0) {
 			return result;
-
 		}
-		int length = nums.length;
 
-		int start = nums[0];
-		int end = 0;
+		int start = nums[0]; // Start of the current range
 
-		for (int i = 0; i < length - 1; i++) {
-			int j = nums[i];
-			int k = nums[i + 1];
-
-			if (k != j + 1) {
-
-				if (k == start) {
+		for (int i = 1; i < nums.length; i++) {
+			// If the current number is not consecutive
+			if (nums[i] != nums[i - 1] + 1) {
+				// Add the range to the result
+				if (start == nums[i - 1]) {
 					result.add(String.valueOf(start));
-
 				} else {
-
-					end = j;
-					result.add(start + "->" + end);
-					start = k;
+					result.add(start + "->" + nums[i - 1]);
 				}
-
+				// Start a new range
+				start = nums[i];
 			}
-
 		}
 
-		if (nums[length-1] != start + 1) {
-			result.add(String.valueOf(nums[length-1]));
-
+		// Add the last range
+		if (start == nums[nums.length - 1]) {
+			result.add(String.valueOf(start));
 		} else {
-			result.add(start + "->" + nums[length-1]);
-
+			result.add(start + "->" + nums[nums.length - 1]);
 		}
 
 		return result;
 	}
 
 	public static void main(String[] args) {
-		SummaryRanges solution = new SummaryRanges();
+		SummaryRanges2 solution = new SummaryRanges2();
 
 		// Test cases
 		System.out.println(solution.summaryRanges(new int[] { 0, 1, 2, 4, 5, 7 })); // Output: ["0->2", "4->5", "7"]
